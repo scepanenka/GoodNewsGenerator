a98fb02dc3f27c0942b02141bdcb5e63d6404009
Aliaksandr Sciepanienka <scepanenka@gmail.com>
2019-09-27 16:56:53 +0300

    feat: create entity-models

---
+++ GoodNews.Data/Entities/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoodNews.Data.Entities
+{
+    public class Category : Entity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public virtual List<Article> News { get; set; }
+
+    }
+}
---
+++ GoodNews.Data/Entities/Source.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoodNews.Data.Entities
+{
+    public class Source : Entity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public virtual List<Article> News { get; set; }
+    }
+}
