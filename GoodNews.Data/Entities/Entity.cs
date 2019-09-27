c7ae1ee0ee449474ffba0bc25c0b9c4e25c4cf6f
Aliaksandr Sciepanienka <scepanenka@gmail.com>
2019-09-27 14:47:23 +0300

    init: initial commit

---
+++ GoodNews.Data/Entities/Entity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoodNews.Data.Entities
+{
+    public abstract class Entity
+    {
+        public int Id { get; set; }
+        
+    }
+}
