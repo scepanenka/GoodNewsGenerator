4fd0f8007be179d94d0bc682bcae2f7ce1aef6c1
Aliaksandr Sciepanienka <scepanenka@gmail.com>
2019-10-01 15:22:49 +0300

    feat: add User and Role models

---
+++ GoodNews.Data/Entities/Role.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoodNews.DAL.Entities
+{
+    public class Role : Entity
+    {
+        public string Name { get; set; }
+        public Guid UserId { get; set; }
+        public User User { get; set; }
+        public ICollection<User> Users { get; set; }
+
+        public Role()
+        {
+            Users = new List<User>();
+        }
+
+
+    }
+}
---
+++ GoodNews.Data/Entities/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoodNews.DAL.Entities
+{
+    public class User : Entity
+    {
+        public string Name { get; set; }
+        public string Password { get; set; }
+
+        public Guid RoleId { get; set; }
+        public Role Role { get; set; }
+        public ICollection<Role> Roles { get; set; }
+        public User()
+        {
+            Roles = new List<Role>();
+        }
+
+    }
+}
