f48c47faac3a8c6467c11d48b5f00a4e8a21d688
Aliaksandr Scepanenka <scepanenka@gmail.com>
2019-12-19 17:52:51 +0300

    fix: try fix authorization

---
+++ GoodNews.Client/src/hooks/useUser.jsx
@@ -0,0 +1,51 @@
+import React, { createContext, useContext, useEffect, useState } from 'react';
+
+function getCurrentUser(accessToken) {
+    if (accessToken === 'awesomeAccessToken123456789') {
+        return {
+            name: 'Thomas',
+        };
+    }
+}
+
+const initialState = {
+    user: {},
+    accessToken: undefined,
+};
+
+const UserContext = createContext(initialState);
+
+export function UserProvider({ children }) {
+    const [accessToken, setAccessToken] = useState(localStorage.getItem('access_token'));
+    const [user, setUser] = useState({});
+
+    function handleAccessTokenChange() {
+        const savedAccessToken = localStorage.getItem('access_token');
+        if (!savedAccessToken || savedAccessToken === 'null') {
+            localStorage.setItem('access_token', accessToken);
+        }
+
+        if (!user.name && accessToken) {
+            const user = getCurrentUser(accessToken);
+            setUser(user);
+        } else if (!accessToken) {
+            // Log Out
+            localStorage.removeItem('access_token');
+            setUser({});
+        }
+    }
+
+    useEffect(() => {
+        handleAccessTokenChange();
+    }, [accessToken]);
+
+    return (
+        <UserContext.Provider value={{ user, accessToken, setAccessToken }}>
+            <>
+                {children}
+            </>
+        </UserContext.Provider>
+    );
+}
+
+export const useUser = () => useContext(UserContext);
